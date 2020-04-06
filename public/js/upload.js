711ed1d21807bbb7822392f971b1bcdc61bf3e96
Ben Faerber <benfaerber@protonmail.com>
2020-04-05 18:02:40 -0600

    Added basic functionality

---
+++ public/js/upload.js
@@ -0,0 +1,65 @@
+$(() => {
+	$('#upload-btn').click(() => {
+    onUpload();
+		$.ajax({
+			url: '/upload',
+			type: 'POST',
+			data: new FormData($('form')[0]),
+			cache: false,
+			contentType: false,
+			processData: false,
+
+			xhr: () => {
+				let xhr = $.ajaxSettings.xhr();
+				if (xhr.upload) {
+					xhr.upload.addEventListener(
+						'progress',
+						(e) => {
+							if (e.lengthComputable) {
+								uploading(e.loaded, e.total);
+							}
+						},
+						false
+					);
+				}
+				return xhr;
+			}
+		});
+	});
+});
+
+function onUpload() {
+  $('#uploading').show();
+  $('#upload-bar').removeClass('bg-success');
+  $('#upload-complete').text('');
+}
+
+function onComplete() {
+  $('#upload-bar').addClass('bg-success');
+  $('#upload-complete').text('Upload complete!');
+}
+
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) return '0 Bytes';
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+function uploading(loaded, total) {
+	let percent = (loaded / total) * 100;
+	percent = percent.toFixed(2).toString() + "%";
+  $('#upload-bar').width(percent);
+  $("#upload-percentage").text(percent);
+
+  const loadedSize = formatBytes(loaded);
+  const totalSize = formatBytes(total);
+
+  $('#upload-size').text(`${loadedSize} / ${totalSize}`);
+
+  if (Math.floor(loaded / total) === 1) {
+    onComplete();
+  }
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const upload = require('express-fileupload');
 const app = express();
 
 app.use('/static', express.static('public'));
@@ -13,16 +14,40 @@ app.use(
 		proxy: true,
 		cookie: {
 			path: '/',
-			secure: false, 
-			sameSite : 'none',
-			httpOnly: false 
+			secure: false,
+			sameSite: 'none',
+			httpOnly: false
 		}
 	})
 );
 
+app.use(
+	upload({
+		useTempFiles: true,
+		tempFileDir: '/tmp/',
+		limits: { fileSize: 50 * 1024 * 1024 },
+		debug: false
+	})
+);
+
+app.set('view engine', 'ejs');
+
 app.get('/', async (req, res) => {
-  res.end('test');
+	res.render('index');
 });
 
+app.post('/upload', (req, res) => {
+	if (!req.files || Object.keys(req.files).length === 0) {
+		return res.status(400).send('No files were uploaded.');
+	}
+
+	let file = req.files.file;
+
+	file.mv(`./files/${file.name}`, (err) => {
+		if (err) return res.status(500).send(err);
+
+		res.send('File uploaded!');
+	});
+});
 
 app.listen(8080);
